4b948ff50f51a8b1b409617fb58c956e1e579518
Daniel B <busisd@carleton.edu>
2019-04-20 22:11:33 -0500

    Implemented the GridNode class

--- SearchVis.py
+++ SearchVis.py
@@ -33,7 +33,7 @@ class PriorityNode(object):
 		return self.priority < other.priority		
 			
 class NodeGrid:
-	def __init__(total_rows, total_cols, win, start_pos=(0,0), end_pos = None, blocks=0):
+	def __init__(self, total_rows, total_cols, win, start_pos=(0,0), end_pos = None, blocks=0):
 		self.rows = total_rows
 		self.cols = total_cols
 		self.nodes = [VisNode(r, c) for c in range(total_cols) for r in range(total_rows)]
@@ -42,35 +42,33 @@ class NodeGrid:
 		if end_pos is None:
 			end_pos = (self.rows-1, self.cols-1)
 		self.start_node = self.nodes[start_pos[0] + self.rows*start_pos[1]]
+		self.start_node.setColor("red")
+		self.start_node.visited = True
 		self.end_node = self.nodes[end_pos[0] + self.rows*end_pos[1]]
-		
-	def make_blocks(blocks):
-		for i in range(blocks):
-			r = random.randrange(0,total_rows)
-			c = random.randrange(0,total_cols)
-			n = nodes[r + total_rows*c]
-			if (n is not start_node and n is not end_node):
-				n.setColor("black")
-				n.visited = True
+		self.end_node.setColor("green")
 
 		
-def perform_search(nodes, priority_funct, start_pos = (2,10), end_pos = (6,3), sleep_time = .01, blocks = 0):
-	start_node = nodes[start_pos[0] + total_rows*start_pos[1]]
-	start_node.setColor("red")
-	start_node.visited = True
-	end_node = nodes[end_pos[0] + total_rows*end_pos[1]]
-	end_node.setColor("green")
-	
-	for i in range(blocks):
-		r = random.randrange(0,total_rows)
-		c = random.randrange(0,total_cols)
-		n = nodes[r + total_rows*c]
-		if (n is not start_node and n is not end_node):
-			n.setColor("black")
-			n.visited = True
-			
+	def make_blocks(self, blocks):
+		for i in range(blocks):
+			r = random.randrange(0,self.rows)
+			c = random.randrange(0,self.cols)
+			n = nodes[r + total_rows*c]
+			if (n is not self.start_node and n is not self.end_node):
+				n.setColor("black")
+				n.visited = True
+				
+	def draw_all(self):
+		for node in self.nodes:
+			node.draw(win)
+
+	def undraw_all(self):
+		for node in self.nodes:
+			node.undraw()
+
+		
+def perform_search(grid, priority_funct, start_pos = (2,10), end_pos = (6,3), sleep_time = .01, blocks = 0):	
 	q = PriorityQueue()
-	q.put(PriorityNode(0,start_node))
+	q.put(PriorityNode(0,grid.start_node))
 	red = 255
 	green = 0
 	blue = 255
@@ -78,37 +76,37 @@ def perform_search(nodes, priority_funct, start_pos = (2,10), end_pos = (6,3), s
 	while(not q.empty()):
 		cur_node = q.get().node
 		
-		if (cur_node == end_node):
-			end_node.setColor(color_rgb(100,255,100))
+		if (cur_node == grid.end_node):
+			grid.end_node.setColor(color_rgb(100,255,100))
 			break
 		
 		cur_node.setColor(color_rgb(int(red),int(green),int(blue)))
 		red = red*.995
 		blue = blue*.995
 		for r,c in ((cur_node.row, cur_node.col-1), (cur_node.row, cur_node.col+1), (cur_node.row-1, cur_node.col), (cur_node.row+1, cur_node.col)): 
-			if (r >= 0 and c >= 0 and r < total_rows and c < total_cols):
-				add_node = nodes[r + total_rows*c]
+			if (r >= 0 and c >= 0 and r < grid.rows and c < grid.cols):
+				add_node = grid.nodes[r + total_rows*c]
 				if (not add_node.visited):
 					add_node.visited = True
 					nodes_visited += 1
-					if (add_node != end_node):
+					if (add_node != grid.end_node):
 						add_node.setColor(color_rgb(220,255,220))
 					else: 
 						add_node.setColor(color_rgb(30,180,30))
 					
-					add_node_priority = priority_funct(start_node, end_node, nodes_visited, add_node)
+					add_node_priority = priority_funct(grid, nodes_visited, add_node)
 					q.put(PriorityNode(add_node_priority, add_node))
 
 		if (sleep_time > 0):
 			time.sleep(sleep_time)	
 
-def greedy_priority(start_node, end_node, nodes_visited, add_node):
-	return abs(end_node.row-add_node.row)+abs(end_node.col-add_node.col)
+def greedy_priority(grid, nodes_visited, add_node):
+	return abs(grid.end_node.row-add_node.row)+abs(grid.end_node.col-add_node.col)
 
-def bfs_priority(start_node, end_node, nodes_visited, add_node):
+def bfs_priority(grid, nodes_visited, add_node):
 	return nodes_visited
 
-def dfs_priority(start_node, end_node, nodes_visited, add_node):
+def dfs_priority(grid, nodes_visited, add_node):
 	return total_rows*total_cols-nodes_visited
 			
 if __name__ == "__main__":
@@ -118,16 +116,13 @@ if __name__ == "__main__":
 	
 	key_val = None
 	while (key_val != "x"):	
-		nodes = [VisNode(r, c) for c in range(total_cols) for r in range(total_rows)]
-		for node in nodes:
-			node.draw(win)
-			
-		perform_search(nodes, greedy_priority, start_pos = (17,17), blocks=0)
-	
+		grid = NodeGrid(total_rows, total_cols, win)
+		grid.draw_all()
+		perform_search(grid, greedy_priority, start_pos = (17,17), blocks=0)
+		
 		key_val = win.getKey()
 		if (key_val != "x"):
-			for node in nodes:
-				node.undraw()
+			grid.undraw_all()
 		
 	win.close()
 	
