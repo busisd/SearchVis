07c16d19176c6856fc1864f73c480f21edbbdbaa
Daniel B <busisd@carleton.edu>
2019-04-21 02:26:49 -0500

    Cells now display their length along the path.

--- SearchVis.py
+++ SearchVis.py
@@ -15,14 +15,22 @@ class VisNode:
 		self.vis_rect.setOutline("black")
 		self.vis_rect.setFill("white")
 		
+		self.path_distance = 0
+		self.text = Text(self.vis_rect.getCenter(), str(self.path_distance))
+		
 	def draw(self, win):
 		self.vis_rect.draw(win)
+		self.text.draw(win)
 
 	def undraw(self):
 		self.vis_rect.undraw()
+		self.text.undraw()
 		
 	def setColor(self, color):
 		self.vis_rect.setFill(color)
+		
+	def update_text(self):
+		self.text.setText(str(self.path_distance))
 
 class PriorityNode(object):
 	def __init__(self, priority, node):
@@ -33,12 +41,10 @@ class PriorityNode(object):
 		return self.priority < other.priority		
 			
 class NodeGrid:
-	def __init__(self, total_rows, total_cols, win, start_pos=(0,0), end_pos = None, blocks=0):
+	def __init__(self, total_rows, total_cols, start_pos=(0,0), end_pos = None, blocks=0):
 		self.rows = total_rows
 		self.cols = total_cols
 		self.nodes = [VisNode(r, c) for c in range(total_cols) for r in range(total_rows)]
-		if (blocks > 0):
-			pass #self.make_blocks(blocks)
 		if end_pos is None:
 			end_pos = (self.rows-1, self.cols-1)
 		self.start_node = self.nodes[start_pos[0] + self.rows*start_pos[1]]
@@ -46,18 +52,26 @@ class NodeGrid:
 		self.start_node.visited = True
 		self.end_node = self.nodes[end_pos[0] + self.rows*end_pos[1]]
 		self.end_node.setColor("green")
+		if (blocks > 0):
+			self.make_random_blocks(blocks)
 
 		
-	def make_blocks(self, blocks):
+	def make_random_blocks(self, blocks):
 		for i in range(blocks):
 			r = random.randrange(0,self.rows)
 			c = random.randrange(0,self.cols)
-			n = nodes[r + total_rows*c]
+			n = self.nodes[r + total_rows*c]
 			if (n is not self.start_node and n is not self.end_node):
 				n.setColor("black")
 				n.visited = True
 				
-	def draw_all(self):
+	def add_blocks_at(self, block_pos_list):
+		for pos in block_pos_list:
+			node = self.nodes[pos[0] + self.rows*pos[1]]
+			node.setColor("black")
+			node.setVisited = True
+			
+	def draw_all(self, win):
 		for node in self.nodes:
 			node.draw(win)
 
@@ -66,7 +80,7 @@ class NodeGrid:
 			node.undraw()
 
 		
-def perform_search(grid, priority_funct, start_pos = (2,10), end_pos = (6,3), sleep_time = .01, blocks = 0):	
+def perform_search(grid, priority_funct, sleep_time = .01):	
 	q = PriorityQueue()
 	q.put(PriorityNode(0,grid.start_node))
 	red = 255
@@ -88,6 +102,8 @@ def perform_search(grid, priority_funct, start_pos = (2,10), end_pos = (6,3), sl
 				add_node = grid.nodes[r + total_rows*c]
 				if (not add_node.visited):
 					add_node.visited = True
+					add_node.path_distance = cur_node.path_distance+1
+					add_node.update_text()
 					nodes_visited += 1
 					if (add_node != grid.end_node):
 						add_node.setColor(color_rgb(220,255,220))
@@ -96,6 +112,9 @@ def perform_search(grid, priority_funct, start_pos = (2,10), end_pos = (6,3), sl
 					
 					add_node_priority = priority_funct(grid, nodes_visited, add_node)
 					q.put(PriorityNode(add_node_priority, add_node))
+				elif (add_node.path_distance>cur_node.path_distance+1):
+					add_node.path_distance = cur_node.path_distance+1
+					add_node.update_text()
 
 		if (sleep_time > 0):
 			time.sleep(sleep_time)	
@@ -108,17 +127,21 @@ def bfs_priority(grid, nodes_visited, add_node):
 
 def dfs_priority(grid, nodes_visited, add_node):
 	return total_rows*total_cols-nodes_visited
-			
+
+def a_star_priority(grid, nodes_visited, add_node):
+	return abs(grid.end_node.row-add_node.row)+abs(grid.end_node.col-add_node.col)-add_node.path_distance
+
 if __name__ == "__main__":
-	total_rows = 20
-	total_cols = 20
+	total_rows = 5
+	total_cols = 5
 	win = GraphWin("Search Visualization", total_rows*node_width+1, total_cols*node_height+1)
 	
 	key_val = None
 	while (key_val != "x"):	
-		grid = NodeGrid(total_rows, total_cols, win)
-		grid.draw_all()
-		perform_search(grid, greedy_priority, start_pos = (17,17), blocks=0)
+		grid = NodeGrid(total_rows, total_cols, blocks = 5)
+		grid.draw_all(win)
+		
+		perform_search(grid, greedy_priority, sleep_time=.5)
 		
 		key_val = win.getKey()
 		if (key_val != "x"):
